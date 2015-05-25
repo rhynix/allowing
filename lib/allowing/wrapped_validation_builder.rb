fa4ec30f28bb5cb0eafa7766ce968849cf4a7de6
Rik Tonnard <rik.tonnard@me.com>
2015-05-25 12:12:40 +0200

    Refactor WrappedValidationBuilder some more

--- lib/allowing/wrapped_validation_builder.rb
+++ lib/allowing/wrapped_validation_builder.rb
@@ -1,7 +1,8 @@
-require 'allowing/extensions/string'
 require 'allowing/wrapping_builder'
 require 'allowing/validation_builder'
 require 'allowing/validations/block_validation'
+require 'allowing/validation_dsl'
+require 'allowing/validations_group'
 
 module Allowing
   class WrappedValidationBuilder
@@ -14,6 +15,12 @@ module Allowing
     end
 
     def build
+      WrappingBuilder.new(unwrapped_validation, wrappers).build
+    end
+
+    private
+
+    def unwrapped_validation
       return build_simple_validations if simple_validations?
       return build_nested_validations if nested_validations?
       return build_block_validation   if block_validations?
@@ -21,37 +28,24 @@ module Allowing
       fail ArgumentError, 'Wrong argument combination given'
     end
 
-    private
-
-    def build_bare_validations
-      validations.map { |type, rule| ValidationBuilder.new(type, rule).build }
-    end
-
     def build_block_validation
-      bare_validation = Validations::BlockValidation.new(&@block)
-      WrappingBuilder.new(bare_validation, wrappers).build
+      Validations::BlockValidation.new(&@block)
     end
 
     def build_nested_validations
       add_attributes_wrapper
 
-      validations = ValidationDSL.define(&@block)
-      validation  = group_validations(validations)
-
-      WrappingBuilder.new(validation, wrappers).build
+      group_validations(ValidationDSL.define(&@block))
     end
 
     def build_simple_validations
       add_attributes_wrapper
 
-      bare_validation = group_validations(build_bare_validations)
-      WrappingBuilder.new(bare_validation, wrappers).build
+      group_validations(ungrouped_validations)
     end
 
-    def wrap_validations(validations)
-      validations.map do |validation|
-        WrappingBuilder.new(validation, wrappers).build
-      end
+    def ungrouped_validations
+      validations.map { |type, rule| ValidationBuilder.new(type, rule).build }
     end
 
     def group_validations(validations)
