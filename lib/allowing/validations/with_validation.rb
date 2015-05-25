c8e674af3d1d185ee1fff63188cd412632fa715d
Rik Tonnard <rik.tonnard@me.com>
2015-05-25 18:00:14 +0200

    Remove alias for rule in with validation

--- lib/allowing/validations/with_validation.rb
+++ lib/allowing/validations/with_validation.rb
@@ -3,10 +3,8 @@ require 'allowing/validations/validation'
 module Allowing
   module Validations
     class WithValidation < Validation
-      alias_method :validator, :rule
-
       def validate(value, _subject, errors)
-        validator.new(value).validate(errors)
+        rule.new(value).validate(errors)
       end
     end
   end
